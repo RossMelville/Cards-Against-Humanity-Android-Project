8c035512bb54933e9f87550eebfbd378b045918e
Ross Melville <rossmelville@hotmail.com>
2017-08-11 14:59:09 +0100

    Added Deck class with constructor.

---
+++ app/src/main/java/com/example/cardsagainsthumanity/Deck.java
@@ -0,0 +1,18 @@
+package com.example.cardsagainsthumanity;
+
+import java.util.ArrayList;
+
+/**
+ * Created by rossmelville on 11/08/2017.
+ */
+
+public class Deck {
+
+    private ArrayList<Card> deck;
+
+    public Deck() {
+        deck = new ArrayList<Card>();
+    }
+
+    
+}
