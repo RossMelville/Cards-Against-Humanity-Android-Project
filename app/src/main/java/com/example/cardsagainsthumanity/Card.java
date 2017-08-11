dd083ee163a8eb46963f34bf7a37754ffc96a164
Ross Melville <rossmelville@hotmail.com>
2017-08-11 14:41:14 +0100

    Initial commit.  Created CardsAgainstHumanity project and added a Card class.

---
+++ app/src/main/java/com/example/cardsagainsthumanity/Card.java
@@ -0,0 +1,15 @@
+package com.example.cardsagainsthumanity;
+
+/**
+ * Created by rossmelville on 11/08/2017.
+ */
+
+public class Card {
+
+    private String statement;
+
+    public Card(String statement) {
+        this.statement =  statement;
+    }
+
+}
