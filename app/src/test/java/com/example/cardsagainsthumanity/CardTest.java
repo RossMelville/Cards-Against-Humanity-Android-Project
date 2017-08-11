6e227a2442845bf04d1a973a205bba0e5183c0d4
Ross Melville <rossmelville@hotmail.com>
2017-08-11 14:46:56 +0100

    Added a CardTest class and test to ensure card statement can be returned.

---
+++ app/src/test/java/com/example/cardsagainsthumanity/CardTest.java
@@ -0,0 +1,18 @@
+package com.example.cardsagainsthumanity;
+
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+
+/**
+ * Created by rossmelville on 11/08/2017.
+ */
+
+public class CardTest {
+
+    @Test
+    public void canReturnStatement() {
+        Card card = new Card("Pulling out");
+        assertEquals("Pulling out", card.getStatement);
+    }
+}
