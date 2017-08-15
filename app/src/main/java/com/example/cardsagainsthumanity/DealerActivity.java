3321f113b07c7f8ff91c4e64b6f70016a51b3c00
Ross Melville <rossmelville@hotmail.com>
2017-08-15 16:31:12 +0100

    Added to the DealerActivity to allow the funniest answer to be selected before moving to the next round, MVP met.

--- app/src/main/java/com/example/cardsagainsthumanity/DealerActivity.java
+++ app/src/main/java/com/example/cardsagainsthumanity/DealerActivity.java
@@ -1,5 +1,6 @@
 package com.example.cardsagainsthumanity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -26,25 +27,33 @@ public class DealerActivity extends AppCompatActivity {
         TextView statement = (TextView) findViewById(R.id.statement);
         statement.setText(game.getBlackStatementCards().get(0).getStatement());
 
+        listView = (ListView) findViewById(R.id.list);
+
         String[] values = new String[game.getPlayers().size()];
+
         for(int i = 0; i < game.getWhiteAnswerCards().size(); i++) {
             values[i] = game.getWhiteAnswerCards().get(i).getStatement();
         }
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>
+        final ArrayAdapter<String> adapter = new ArrayAdapter<String>
                 (this, android.R.layout.simple_list_item_1, android.R.id.text1, values);
 
         listView.setAdapter(adapter);
 
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void OnItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
                 int itemPosition = i;
                 String item = (String) listView.getItemAtPosition(i);
                 Player winningPlayer = game.getPlayers().get(itemPosition);
                 game.addPoint(winningPlayer);
                 game.blackStatementCards.clear();
                 game.whiteAnswerCards.clear();
+                game.setDealer(0);
+
+                Intent intent = new Intent(DealerActivity.this, GameActivity.class);
+                intent.putExtra("game", game);
+                startActivity(intent);
 
             }
 
