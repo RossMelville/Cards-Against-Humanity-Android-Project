7555825cc924afab374aff7dd09d2a73944c445e
Ross Melville <rossmelville@hotmail.com>
2017-08-16 22:13:51 +0100

    Added a toast to the DealerChooseActivity

--- app/src/main/java/com/example/cardsagainsthumanity/DealerChooseActivity.java
+++ app/src/main/java/com/example/cardsagainsthumanity/DealerChooseActivity.java
@@ -1,6 +1,7 @@
 package com.example.cardsagainsthumanity;
 
 import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -8,6 +9,7 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class DealerChooseActivity extends AppCompatActivity {
 
@@ -45,15 +47,26 @@ public class DealerChooseActivity extends AppCompatActivity {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
                 int cardPosition = i;
-                Card selectedCard =  game.blackStatementCards.get(cardPosition);
+                Card selectedCard = game.blackStatementCards.get(cardPosition);
                 game.blackStatementCards.clear();
                 game.addBlackStatementCard(selectedCard);
 
-                Intent intent = new Intent(DealerChooseActivity.this, GameActivity.class);
-                intent.putExtra("game", game);
-                startActivity(intent);
+                if (game.getDealerIndex() == 0) {
+                    Toast.makeText(DealerChooseActivity.this, "Pass to " + game.players.get(1).getName(), Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(DealerChooseActivity.this, "Pass to " + game.players.get(0).getName(), Toast.LENGTH_LONG).show();
+                }
+                Handler toastHandler = new Handler();
+                toastHandler.postDelayed(new Runnable() {
+
+                    @Override
+                    public void run() {
+                        Intent intent = new Intent(DealerChooseActivity.this, GameActivity.class);
+                        intent.putExtra("game", game);
+                        startActivity(intent);
+                    }
+                }, 2000L);
             }
         });
-
     }
 }
